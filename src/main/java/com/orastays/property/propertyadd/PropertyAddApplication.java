2cbd072e7ba66cb3f0325b87a2332a793db30534
Avirup.Pal <Avirup.Pal@cognizant.com>
2018-12-01 22:49:21 +0530

    first commit

---
+++ src/main/java/com/orastays/property/propertyadd/PropertyAddApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.property.propertyadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyAddApplication.class, args);
+	}
+}
