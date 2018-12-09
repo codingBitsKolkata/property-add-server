b8f74ddbec89c615aac4a8aa3f504daeebbd6db8
Shibu <shibu_sxc@yahoo.co.in>
2018-12-09 17:10:47 +0530

    Model Update

--- src/main/java/com/orastays/property/propertyadd/validation/PropertyValidation.java
+++ src/main/java/com/orastays/property/propertyadd/validation/PropertyValidation.java
@@ -2,6 +2,7 @@ package com.orastays.property.propertyadd.validation;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.transaction.Transactional;
 
@@ -14,6 +15,8 @@ import com.orastays.property.propertyadd.exceptions.FormExceptions;
 import com.orastays.property.propertyadd.helper.PropertyAddConstant;
 import com.orastays.property.propertyadd.helper.Util;
 import com.orastays.property.propertyadd.model.CommonModel;
+import com.orastays.property.propertyadd.model.PropertyModel;
+import com.orastays.property.propertyadd.model.UserModel;
 
 @Component
 @Transactional
@@ -65,5 +68,129 @@ public class PropertyValidation extends AuthorizeUserValidation {
 		if (logger.isDebugEnabled()) {
 			logger.debug("validateLanguageWithUserToken -- End");
 		}
+	}
+	
+	public UserModel validatePropertyAdd(PropertyModel propertyModel) throws FormExceptions {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("validatePropertyAdd -- Start");
+		}
+
+		Util.printLog(propertyModel, PropertyAddConstant.INCOMING, "Property Add", request);
+
+		Map<String, Exception> exceptions = new LinkedHashMap<>();
+		UserModel userModel = null;
+		
+		if(Objects.nonNull(propertyModel)) {
+			
+			// Validate Property Name
+			if(StringUtils.isBlank(propertyModel.getName())) {
+				exceptions.put(messageUtil.getBundle("property.name.null.code"), new Exception(messageUtil.getBundle("property.name.null.message")));
+			} else {
+				if(!Util.checkAlphabet(propertyModel.getName())) {
+					exceptions.put(messageUtil.getBundle("property.name.invalid.code"), new Exception(messageUtil.getBundle("property.name.invalid.message")));
+				}
+			}
+			
+			// Calculation Pending
+			propertyModel.setOraname("ORA00001");
+			
+			//validate Entire Apartment
+			if(StringUtils.isBlank(propertyModel.getEntireApartment())) {
+				exceptions.put(messageUtil.getBundle("entire.appartment.null.code"), new Exception(messageUtil.getBundle("entire.appartment.null.message")));
+			}
+			
+			//Validate Dedicated Space
+			if(StringUtils.isBlank(propertyModel.getDedicatedSpace())) {
+				exceptions.put(messageUtil.getBundle("dedicated.space.null.code"), new Exception(messageUtil.getBundle("dedicated.space.null.message")));
+			}
+			
+			//Validate Latitude
+			if(StringUtils.isBlank(propertyModel.getLatitude())) {
+				exceptions.put(messageUtil.getBundle("latitude.null.code"), new Exception(messageUtil.getBundle("latitude.null.message")));
+			} else {
+				if(!Util.checkLatitude(propertyModel.getLatitude())) {
+					exceptions.put(messageUtil.getBundle("latitude.invalid.code"), new Exception(messageUtil.getBundle("latitude.invalid.message")));
+				}
+			}
+			
+			//Validate Longitude
+			if(StringUtils.isBlank(propertyModel.getLongitude())) {
+				exceptions.put(messageUtil.getBundle("longitude.null.code"), new Exception(messageUtil.getBundle("longitude.null.message")));
+			} else {
+				if(!Util.checkLongitude(propertyModel.getLongitude())) {
+					exceptions.put(messageUtil.getBundle("longitude.invalid.code"), new Exception(messageUtil.getBundle("longitude.invalid.message")));
+				}
+			}
+			
+			//Validate Address
+			if(StringUtils.isBlank(propertyModel.getAddress())) {
+				exceptions.put(messageUtil.getBundle("address.null.code"), new Exception(messageUtil.getBundle("address.null.message")));
+			}
+			
+			//Validate Start End Date
+			if(StringUtils.isBlank(propertyModel.getStartDate())) {
+				exceptions.put(messageUtil.getBundle("start.date.null.code"), new Exception(messageUtil.getBundle("start.date.null.message")));
+			} else {
+				if(StringUtils.isBlank(propertyModel.getEndDate())) {
+					exceptions.put(messageUtil.getBundle("end.date.null.code"), new Exception(messageUtil.getBundle("end.date.null.message")));
+				} else {
+					
+					// logic for date difference
+				}
+			}
+			
+			//Validate CheckIn Time
+			if(StringUtils.isBlank(propertyModel.getCheckinTime())) {
+				exceptions.put(messageUtil.getBundle("checkin.time.null.code"), new Exception(messageUtil.getBundle("checkin.time.null.message")));
+			} else {
+				if(!Util.checkTimeFormat(propertyModel.getCheckinTime())){
+					exceptions.put(messageUtil.getBundle("checkin.time.invalid.code"), new Exception(messageUtil.getBundle("checkin.time.invalid.message")));
+				}
+			}
+			
+			//Validate CheckOut Time
+			if(StringUtils.isBlank(propertyModel.getCheckoutTime())) {
+				exceptions.put(messageUtil.getBundle("checkout.time.null.code"), new Exception(messageUtil.getBundle("checkout.time.null.message")));
+			} else {
+				if(!Util.checkTimeFormat(propertyModel.getCheckoutTime())){
+					exceptions.put(messageUtil.getBundle("checkout.time.invalid.code"), new Exception(messageUtil.getBundle("checkout.time.invalid.message")));
+				}
+			}
+			
+			//Validate Image Url
+			if(StringUtils.isBlank(propertyModel.getCoverImageUrl())) {
+				exceptions.put(messageUtil.getBundle("image.url.null.code"), new Exception(messageUtil.getBundle("image.url.null.message")));
+			}
+
+			//Validate Price Drop
+			if(StringUtils.isBlank(propertyModel.getPriceDrop())) {
+				exceptions.put(messageUtil.getBundle("price.drop.null.code"), new Exception(messageUtil.getBundle("price.drop.null.message")));
+			}
+			
+			//Validate Property Type
+			if(Objects.nonNull(propertyModel.getPropertyTypeModel())) {
+				exceptions.put(messageUtil.getBundle("propertty.type.null.code"), new Exception(messageUtil.getBundle("propertty.type.null.message")));
+			} else {
+				if(StringUtils.isBlank(propertyModel.getPropertyTypeModel().getPropertyTypeId())) {
+					exceptions.put(messageUtil.getBundle("propertty.type.id.null.code"), new Exception(messageUtil.getBundle("propertty.type.id.null.message")));
+				} else {
+					if(Objects.isNull(propertyTypeDAO.find(Long.parseLong(propertyModel.getPropertyTypeModel().getPropertyTypeId())))) {
+						exceptions.put(messageUtil.getBundle("propertty.type.id.invalid.code"), new Exception(messageUtil.getBundle("propertty.type.id.invalid.message")));
+					}
+				}
+			}
+			
+			// Validate Property Vs Description
+			
+			
+		}
+		
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("validatePropertyAdd -- End");
+		}
+		
+		return userModel;
 	}
 }
